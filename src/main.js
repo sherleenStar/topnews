a8105dbe454daacdd02789bcc42409978172329f
sherleenStar <sherleenStar@163.com>
2019-09-18 09:46:58 +0800

    导入静态资源,处理无用文件,导入igdex.less文件

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import './styles/index.less'
+// import axios from 'axios'
 
+// Vue.prototype.$http = axios
+// axios.defaults.baseURL = 'http://localhost:8080/'
 Vue.config.productionTip = false
+Vue.use(ElementUI)
 
 new Vue({
   router,
